d1d762e4e7f2198d11d4154cc8167c6ee644c338
lucasgustavodiaz <lucasgustavodiaz@hotmail.com>
2021-12-17 16:52:15 -0300

    Footer update

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { Icon } from '@components/icons';
@@ -68,10 +68,10 @@ const StyledCredit = styled.div`
 `;
 
 const Footer = () => {
-  const [githubInfo, setGitHubInfo] = useState({
-    stars: null,
-    forks: null,
-  });
+  // const [githubInfo, setGitHubInfo] = useState({
+  //   stars: null,
+  //   forks: null,
+  // });
 
   useEffect(() => {
     if (process.env.NODE_ENV !== 'production') {
@@ -79,13 +79,13 @@ const Footer = () => {
     }
     fetch('https://api.github.com/repos/lucasgustavodiaz/lucasdiaz')
       .then(response => response.json())
-      .then(json => {
-        const { stargazers_count, forks_count } = json;
-        setGitHubInfo({
-          stars: stargazers_count,
-          forks: forks_count,
-        });
-      })
+      // .then(json => {
+      //   const { stargazers_count, forks_count } = json;
+      //   setGitHubInfo({
+      //     stars: stargazers_count,
+      //     forks: forks_count,
+      //   });
+      // })
       .catch(e => console.error(e));
   }, []);
 
@@ -108,7 +108,7 @@ const Footer = () => {
         <a href="https://github.com/lucasgustavodiaz/lucasdiaz">
           <div>Diseñado y construido por Lucas Diaz</div>
 
-          {githubInfo.stars && githubInfo.forks && (
+          {/* {githubInfo.stars && githubInfo.forks && (
             <div className="github-stats">
               <span>
                 <Icon name="Star" />
@@ -119,7 +119,7 @@ const Footer = () => {
                 <span>{githubInfo.forks.toLocaleString()}</span>
               </span>
             </div>
-          )}
+          )} */}
         </a>
       </StyledCredit>
     </StyledFooter>
